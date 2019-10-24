3ec346db0b6b0d1d258a3ad58d033c8af2edd2f5
Kevin Pugliese <kevin.lima.contato@gmail.com>
2019-10-23 21:47:20 -0300

    Controller braspag

--- src/main/java/com/payment/integration/controllers/BraspagController.java
+++ src/main/java/com/payment/integration/controllers/BraspagController.java
@@ -1,4 +1,24 @@
 package com.payment.integration.controllers;
 
+import com.payment.integration.facades.BraspagFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/braspag")
 public class BraspagController {
+
+    @Autowired
+    private BraspagFacade braspagFacade;
+
+    @GetMapping(name = "/hello")
+    public String getHello() {
+        System.out.println("Ola mundo");
+
+        String authorization = braspagFacade.authorization();
+
+        return authorization;
+    }
 }
