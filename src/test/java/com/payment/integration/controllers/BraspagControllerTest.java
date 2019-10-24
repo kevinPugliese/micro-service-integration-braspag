5ae6476322a84997208db72433cefb5fe2973347
Kevin Pugliese <kevin.lima.contato@gmail.com>
2019-10-24 18:58:40 -0300

    Tests

--- src/test/java/com/payment/integration/controllers/BraspagControllerTest.java
+++ src/test/java/com/payment/integration/controllers/BraspagControllerTest.java
@@ -1,4 +1,33 @@
 package com.payment.integration.controllers;
 
+import com.payment.integration.facades.BraspagFacade;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = BraspagController.class)
 public class BraspagControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BraspagController braspagController;
+
+    @MockBean
+    private BraspagFacade braspagFacade;
+
+    @Test
+    public void getContextLoad() throws Exception {
+        mockMvc.perform(post("/forums/42/register")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+
+        assertTh
+    }
 }
